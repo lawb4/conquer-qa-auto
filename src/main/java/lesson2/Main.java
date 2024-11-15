a64c9d393e0cd9f5f806ad873db6e87f1e387305
lawb4 <yahorlabanau@gmail.com>
2024-11-15 13:45:09 +0300

    added some stepic java course tasks

---
+++ src/main/java/lesson2/Main.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(evenOrOdd(3));
+    }
+
+    // метод должен возвращать строку "чётное", если число чётное и строку "нечётное", если число нечётное
+    public static String evenOrOdd(int a) {
+        return a % 2 == 0 ? "Чётное" : "Нечётное";
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sqrt(-5);
+    }
+
+    public static double sqrt(double x) throws IllegalArgumentException{
+        if (x < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + x);
+        }
+        return Math.sqrt(x);
     }
 }
