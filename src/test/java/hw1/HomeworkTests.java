f8ec905ab309e8896667722d0abf98294cb25872
lawb4 <yahorlabanau@gmail.com>
2024-11-16 18:24:46 +0300

    fix packages hw counter

--- src/test/java/hw1/HomeworkTests.java
+++ src/test/java/hw1/HomeworkTests.java
@@ -1,8 +1,8 @@
-package hw2;
+package hw1;
 
 import org.junit.jupiter.api.Test;
 
-import static hw2.Tasks.*;
+import static hw1.Tasks.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HomeworkTests {
